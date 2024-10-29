66a7b1753e798612c1aa74aa6b801c6894a9ba9b
Varun <tatireddyvarun20@gmail.com>
2024-10-30 03:59:55 +0530

    Bookmarks API integration

--- src/components/Collection.jsx
+++ src/components/Collection.jsx
@@ -1,322 +1,81 @@
-// import React, { useState, useEffect } from "react";
-// import { useNavigate } from "react-router-dom";
-// import "./Collection.css";
-// import SearchIcon from "../assets/images/Search.svg";
-
-// const Collection = () => {
-//   const [collections, setCollections] = useState([]);
-//   const [selectedCollection, setSelectedCollection] = useState(null);
-//   const [text, setText] = useState("");
-//   const [filteredCollections, setFilteredCollections] = useState([]);
-//   const navigate = useNavigate();
-//   // Fetch collections from localStorage on component mount
-//   //   useEffect(() => {
-//   //     const storedCollections =
-//   //       JSON.parse(localStorage.getItem("collections")) || [];
-//   //     setCollections(storedCollections);
-//   //   }, []);
-
-//   useEffect(() => {
-//     const storedCollections =
-//       JSON.parse(localStorage.getItem("collections")) || [];
-//     setCollections(storedCollections);
-
-//     // Retrieve the selected collection from localStorage
-//     const savedCollectionName = localStorage.getItem("selectedCollectionName");
-//     if (savedCollectionName) {
-//       const savedCollection = storedCollections.find(
-//         (col) => col.name === savedCollectionName
-//       );
-//       setSelectedCollection(savedCollection);
-//     }
-//   }, []);
-
-//   // Handle collection selection
-//   const handleSelectCollection = (collectionName) => {
-//     const collection = collections.find((col) => col.name === collectionName);
-//     setSelectedCollection(collection);
-//   };
-//   //   const handleSearch = (e) => {
-//   //     setText(e.target.value);
-//   //   };
-//   const handleSearch = (e) => {
-//     const searchText = e.target.value;
-//     setText(searchText);
-
-//     // Filter collections based on search text
-//     const filteredCollections = collections.filter((collection) =>
-//       collection.name.toLowerCase().includes(searchText.toLowerCase())
-//     );
-//     setFilteredCollections(filteredCollections);
-
-//     // If a collection is selected, filter articles within it
-//     // if (selectedCollection) {
-//     //   const filteredArticles = selectedCollection.articles.filter((article) =>
-//     //     article.toLowerCase().includes(searchText.toLowerCase())
-//     //   );
-//     //   setFilteredArticles(filteredArticles);
-//     // }
-//   };
-
-//   const handleNavigate = () => {
-//     navigate("/article/");
-//   };
-//   return (
-//     <div className="collection-container">
-//       <header className="Collection-header">
-//         <h3 className="collection-heading">My Collections</h3>
-//         <div className="Search-Collection">
-//           <img
-//             src={SearchIcon}
-//             alt="search"
-//             className="Search-collection-icon"
-//           />
-//           <input
-//             type="text"
-//             value={text}
-//             onChange={handleSearch}
-//             autoFocus
-//             placeholder="Search..."
-//             className="Search-collection-input"
-//           />
-//         </div>
-//       </header>
-//       <div className="collection-list">
-//         <div className="new-collection">
-//           {filteredCollections.length > 0 ? (
-//             filteredCollections.map((collection) => (
-//               <div
-//                 key={collection.name}
-//                 className="collection-item"
-//                 onClick={() => handleSelectCollection(collection.name)}
-//               >
-//                 <p
-//                   style={{
-//                     fontSize: "16px",
-//                     fontWeight: "600",
-//                     fontFamily: "manrope",
-//                     // margin: "0",
-//                   }}
-//                 >
-//                   {collection.name}
-//                 </p>
-//                 <p
-//                   style={{
-//                     fontSize: "14px",
-//                     fontFamily: "manrope",
-//                     // margin: "0",
-//                   }}
-//                 >
-//                   {collection.articles.length} articles
-//                 </p>
-//               </div>
-//             ))
-//           ) : (
-//             <p>No collections found.</p>
-//           )}
-//         </div>
-//         <div className="list">
-//           {selectedCollection && (
-//             <div className="collection-articles">
-//               <div
-//                 className="collection-articles-header"
-//                 style={{ display: "flex", justifyContent: "center" }}
-//               >
-//                 {/* <button
-//                   className="back-button"
-//                   onClick={() => setSelectedCollection(null)}
-//                 >
-//                   Back
-//                 </button> */}
-//                 <h4>Articles in {selectedCollection.name}</h4>
-//               </div>
-//               <ul>
-//                 {selectedCollection.articles.map((articleId) => (
-//                   <li key={articleId}>
-//                     <p>ID: {articleId}</p>
-//                   </li>
-//                 ))}
-//               </ul>
-//             </div>
-//           )}
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Collection;
 import React, { useState, useEffect } from "react";
-//import { IoCloseOutline } from "react-icons/io5";
+import axios from "axios";
 import SearchIcon from "../assets/images/Search.svg";
 import "./Collection.css";
-
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { useParams, useLocation } from "react-router-dom";
 const Collection = () => {
-  const mockCollections = [
-    {
-      name: "Science Articles",
-      articles: [
-        {
-          id: "001",
-          title: "Genomic Insights into Human Evolution",
-          source: "PubMed",
-        },
-        {
-          id: "002",
-          title: "CRISPR Technology in Modern Medicine",
-          source: "PLOS",
-        },
-        { id: "003", title: "Advances in Quantum Biology", source: "BioRxiv" },
-        {
-          id: "004",
-          title: "Climate Change and Ecosystem Shifts",
-          source: "PubMed",
-        },
-        {
-          id: "005",
-          title: "Artificial Intelligence in Biochemistry",
-          source: "PLOS",
-        },
-        {
-          id: "006",
-          title: "New Frontiers in Neuroscience",
-          source: "BioRxiv",
-        },
-        {
-          id: "007",
-          title: "Applications of Microbiome Research",
-          source: "PubMed",
-        },
-        {
-          id: "008",
-          title: "Bioinformatics and Protein Structure",
-          source: "PLOS",
-        },
-        { id: "009", title: "Vaccines and Immune Response", source: "BioRxiv" },
-      ],
-    },
-    {
-      name: "Science Articles - Part II",
-      articles: [
-        { id: "010", title: "Genetic Markers of Disease", source: "PubMed" },
-        {
-          id: "011",
-          title: "Stem Cell Therapies for Cancer",
-          source: "BioRxiv",
-        },
-        {
-          id: "012",
-          title: "Impacts of CRISPR on Gene Editing",
-          source: "PLOS",
-        },
-        {
-          id: "013",
-          title: "Environmental DNA Applications",
-          source: "PubMed",
-        },
-      ],
-    },
-    {
-      name: "Technology Insights",
-      articles: [
-        {
-          id: "014",
-          title: "Blockchain in Health Data Security",
-          source: "PLOS",
-        },
-        {
-          id: "015",
-          title: "Quantum Computing for Drug Discovery",
-          source: "PubMed",
-        },
-        {
-          id: "016",
-          title: "5G and Healthcare Innovations",
-          source: "BioRxiv",
-        },
-      ],
-    },
-    {
-      name: "Health & Wellness",
-      articles: [
-        {
-          id: "017",
-          title: "Mental Health Trends in the Modern Era",
-          source: "PubMed",
-        },
-        {
-          id: "018",
-          title: "Nutritional Science and Longevity",
-          source: "PLOS",
-        },
-      ],
-    },
-    {
-      name: "Health & Wellness - Expanded",
-      articles: [
-        {
-          id: "019",
-          title: "Physical Exercise and Cardiovascular Health",
-          source: "BioRxiv",
-        },
-        {
-          id: "020",
-          title: "Impact of Sleep on Cognitive Function",
-          source: "PubMed",
-        },
-      ],
-    },
-  ];
-
   const [collections, setCollections] = useState([]);
   const [selectedCollection, setSelectedCollection] = useState(null);
   const [text, setText] = useState("");
-  const [filteredCollections, setFilteredCollections] =
-    useState(mockCollections);
-
+  const [filteredCollections, setFilteredCollections] = useState([]);
+  const navigate = useNavigate();
+  const location = useLocation();
+  const { user } = useSelector((state) => state.auth);
+  const token=user?.access_token;
+  const user_id =user?.user_id; // Replace with actual user ID
   useEffect(() => {
-    setCollections(mockCollections);
-    const savedCollectionName = localStorage.getItem("selectedCollectionName");
-    if (savedCollectionName) {
-      const savedCollection = mockCollections.find(
-        (col) => col.name === savedCollectionName
-      );
-      setSelectedCollection(savedCollection);
-    }
-  }, []);
+    const fetchCollections = async () => {
+      try {
+        const response = await axios.get(
+          `http://13.127.207.184:80/bookmarks/${user_id}/collections`,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+        const fetchedCollections = response.data.collections || {};
+        setCollections(fetchedCollections);
+        setFilteredCollections(fetchedCollections);
+
+        const savedCollectionName = localStorage.getItem("selectedCollectionName");
+        if (savedCollectionName && fetchedCollections[savedCollectionName]) {
+          setSelectedCollection({
+            name: savedCollectionName,
+            articles: fetchedCollections[savedCollectionName],
+          });
+        }
+      } catch (error) {
+        console.error("Error fetching collections:", error);
+      }
+    };
+
+    fetchCollections();
+  }, [user_id, token]);
 
   const handleSelectCollection = (collectionName) => {
-    const collection = collections.find((col) => col.name === collectionName);
-    setSelectedCollection(collection);
+    const articles = collections[collectionName];
+    setSelectedCollection({ name: collectionName, articles });
     localStorage.setItem("selectedCollectionName", collectionName);
   };
 
   const handleSearch = (e) => {
     const searchText = e.target.value;
     setText(searchText);
-    const filtered = collections.filter((collection) =>
-      collection.name.toLowerCase().includes(searchText.toLowerCase())
+    const filtered = Object.fromEntries(
+      Object.entries(collections).filter(([name]) =>
+        name.toLowerCase().includes(searchText.toLowerCase())
+      )
     );
     setFilteredCollections(filtered);
   };
-
+  const handleArticleClick = (article_id, source) => {
+    const sourceType = source === "BioRxiv" ? "bioRxiv_id" : source === "Public Library of Science (PLOS)" ? "plos_id" : "pmid";
+    navigate(`/article/${sourceType}:${article_id}`,{
+      state: {
+        
+        annotateData: [],
+      },
+    });
+  };
   return (
     <>
-      {/* <div className="collection-modal"> */}
-      {/* <button
-          className="close-collection"
-          onClick={() => setSelectedCollection(null)}
-        >
-          <IoCloseOutline size={30} color="#1A82FF" />
-        </button> */}
       <div className="collections-list">
         <h3 className="collection-heading">My Collections</h3>
         <div className="Search-Collection">
-          <img
-            src={SearchIcon}
-            alt="search"
-            className="Search-collection-icon"
-          />
+          <img src={SearchIcon} alt="search" className="Search-collection-icon" />
           <input
             type="text"
             value={text}
@@ -325,19 +84,19 @@ const Collection = () => {
             className="Search-collection-input"
           />
         </div>
-        {filteredCollections.length > 0 ? (
-          filteredCollections.map((collection) => (
+        {Object.keys(filteredCollections).length > 0 ? (
+          Object.keys(filteredCollections).map((collectionName) => (
             <div
-              key={collection.name}
+              key={collectionName}
               className={`collection-item ${
-                selectedCollection?.name === collection.name ? "active" : ""
+                selectedCollection?.name === collectionName ? "active" : ""
               }`}
-              onClick={() => handleSelectCollection(collection.name)}
+              onClick={() => handleSelectCollection(collectionName)}
             >
               <div style={{ display: "flex", justifyContent: "space-between" }}>
-                <p style={{ margin: "0", padding: "0" }}>{collection.name}</p>
+                <p style={{ margin: "0", padding: "0" }}>{collectionName}</p>
                 <p style={{ margin: "0", padding: "0" }}>
-                  {collection.articles.length} articles
+                  {filteredCollections[collectionName].length} articles
                 </p>
               </div>
             </div>
@@ -363,48 +122,21 @@ const Collection = () => {
               <tbody>
                 {selectedCollection.articles.map((article) => (
                   <tr
-                    key={article.id} // Use a unique identifier for the key
+                    key={article.article_id}
                     className="article-item"
-                    onClick={() => alert(`Viewing article ${article.id}`)}
-                  >
-                    <td>{article.id}</td>
-                    <td>{article.title}</td>
-                    <td>{article.source}</td>
+                    onClick={() => handleArticleClick(article.article_id, article.article_source)}                  >
+                    <td>{article.article_id}</td>
+                    <td>{article.article_title}</td>
+                    <td>{article.article_source}</td>
                   </tr>
                 ))}
               </tbody>
             </table>
-
-            {/* <ul>
-              {selectedCollection.articles.map((article) => (
-                <li
-                  key={article}
-                  className="article-item"
-                  onClick={() => alert(`Viewing article ${article}`)}
-                >
-                  <div className="item">
-                    <span className="label">PMID:</span>
-                    <span className="value">{article.id}</span>
-                  </div>
-
-                  <div className="item">
-                    <span className="label">Title:</span>
-                    <span className="value">{article.title}</span>
-                  </div>
-
-                  <div className="item">
-                    <span className="label">Source:</span>
-                    <span className="value">{article.source}</span>
-                  </div>
-                </li>
-              ))}
-            </ul> */}
           </>
         ) : (
           <p>Select a collection to view articles.</p>
         )}
       </div>
-      {/* </div> */}
     </>
   );
 };
