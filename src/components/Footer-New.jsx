5f5c492437639bd818ec22e38d1bcfa5fe0e1cea
varunsai20 <tatireddyvarun20@gmail.com>
2025-01-24 12:54:19 +0530

    LandingPage Design update

--- src/components/Footer-New.jsx
+++ src/components/Footer-New.jsx
@@ -1,13 +1,49 @@
 import React from "react";
 
 import "./Footer-New.css";
-
+import { FaXTwitter } from "react-icons/fa6";
+import { FaFacebookF, FaLinkedinIn } from "react-icons/fa";
+import { FaInstagramSquare } from "react-icons/fa";
+import { SlGlobe } from "react-icons/sl";
 const Footer = () => {
   return (
+    <>
+    <div className="socials" style={{marginTop:"auto"}}>
+        <div className="social-icons">
+          <a href="https://x.com/IncInfer" target="_blank" rel="noreferrer">
+            <FaXTwitter size={30} color="black" />
+          </a>
+
+          <a
+            href="https://www.facebook.com/infersol"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FaFacebookF size={30} color="black" />
+          </a>
+          <a
+            href="https://www.linkedin.com/company/infer-solutions/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FaLinkedinIn size={30} color="black" />
+          </a>
+          <a
+            href="https://www.instagram.com/infersol/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FaInstagramSquare size={32} color="black" />
+          </a>
+          <a href="https://www.infersol.com/" target="_blank" rel="noreferrer">
+            <SlGlobe size={27} color="black" />
+          </a>
+        </div>
+      </div>
     <div
       className="footer"
       style={{
-        marginTop: "auto",
+        // marginTop: "auto",
         width: "100%", // Ensure the footer takes up the full width
         textAlign: "center", // Center the content
       }}
@@ -20,6 +56,7 @@ const Footer = () => {
         All Rights Reserved.
       </p>
     </div>
+    </>
   );
 };
 
